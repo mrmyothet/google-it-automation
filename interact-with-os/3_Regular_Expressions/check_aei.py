fcbf987abfd3db89809fe0b095635cda6671b965
Myo Thet <mr.myothet@gmail.com>
2024-10-06 21:50:27 +0700

    regular expression - check_aei

---
+++ interact-with-os/3_Regular_Expressions/check_aei.py
@@ -0,0 +1,12 @@
+import re
+
+
+def check_aei(text):
+    result = re.search(r"a.e.i", text)
+    # print(result)
+    return result != None
+
+
+print(check_aei("academia"))  # True
+print(check_aei("aerial"))  # False
+print(check_aei("paramedic"))  # True
