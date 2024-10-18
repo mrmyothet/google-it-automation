b828e9e7e184540903594f90f0b26b62aadc3e64
Myo Thet <mr.myothet@gmail.com>
2024-10-18 23:43:56 +0700

    extract PID using regex

---
+++ interact-with-os/3_Regular_Expressions/extract_PID_using_regexes.py
@@ -0,0 +1,41 @@
+import re
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
+
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+result = re.search(regex, "A completely different string that also has numbers [34567]")
+print(result[1])
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
+result = re.search(regex, "A completely different string that also has numbers [34567]")
+print(result[1])
+result = re.search(regex, "99 elephants in a [cage]")
+# print(result[1])
+# Note that this print command results in an error as shown in the video.
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+result = re.search(regex, "A completely different string that also has numbers [34567]")
+result = re.search(regex, "99 elephants in a [cage]")
+
+
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]"
+    result = re.search(regex, log_line)
+    if result is None:
+        return ""
+    return result[1]
+
+
+print(extract_pid(log))
+print(extract_pid("99 elephants in a [cage]"))
