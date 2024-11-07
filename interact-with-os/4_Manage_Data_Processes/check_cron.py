9941f0b435d0ee16c20b5026d9a416e3a448fca7
Myo Thet <mr.myothet@gmail.com>
2024-11-07 13:05:13 +0700

    making sense of the data

--- interact-with-os/4_Manage_Data_Processes/check_cron.py
+++ interact-with-os/4_Manage_Data_Processes/check_cron.py
@@ -1,13 +1,22 @@
-#!/bin/env/python3
+#!/usr/bin/env python3
 
 import sys
 import re
 
 log_file = sys.argv[1]
+usernames = {}
+
 with open(log_file) as f:
     for line in f:
         if "CRON" not in line:
             continue
         pattern = r"USER \((.+)\)$"
         result = re.search(pattern, line)
-        print(result)
+
+        if result is None:
+            continue
+
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) + 1
+
+print(usernames)
---
+++ interact-with-os/4_Manage_Data_Processes/usernames.py
@@ -0,0 +1,9 @@
+usernames = {}
+
+name = "good user"
+
+usernames[name] = usernames.get(name, 0) + 1
+print(usernames)
+
+usernames[name] = usernames.get(name, 0) + 1
+print(usernames)
