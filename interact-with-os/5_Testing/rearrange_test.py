ecfd1424fc5acb9dfe39614b524123309f561379
Myo Thet <mr.myothet@gmail.com>
2024-11-08 18:19:28 +0700

    write a basic unittest

---
+++ interact-with-os/5_Testing/rearrange_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+from rearrange import rearrange_name
+
+
+class TestRearrange(unittest.TestCase):
+
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+
+# Run the tests
+unittest.main()
