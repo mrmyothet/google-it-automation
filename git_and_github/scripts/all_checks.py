b73d4e5724eda0d48f3893aa1a71f98888b4a0da
Myo Thet <mr.myothet@gmail.com>
2024-12-03 14:09:57 +0700

    Add check cpu_constrained function

--- git_and_github/scripts/all_checks.py
+++ git_and_github/scripts/all_checks.py
@@ -3,6 +3,7 @@ import os
 import sys
 import shutil
 import socket
+import psutil
 
 
 def check_reboot():
@@ -30,6 +31,11 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise"""
+    return psutil.cpu_percent(1) > 75
+
+
 def check_no_network():
     """Returns True if it fails to resolve Google's URL, False otherwise"""
     try:
@@ -44,6 +50,7 @@ def main():
     checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition is full."),
+        (check_cpu_constrained, "CPU load too high."),
         (check_no_network, "No working network."),
     ]
 
