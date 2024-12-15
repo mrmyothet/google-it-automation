fb12af7d4cd5801400e03c621088fcf270abe917
Myo Thet <mr.myothet@gmail.com>
2024-12-15 20:38:55 +0700

    lab - fixing a slow system using rsync command and ProcessPoolExecutor

--- troubleshooting/slowness/daily_sync.py
+++ troubleshooting/slowness/daily_sync.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 
 import subprocess
-from multiprocessing import Pool
+
+# from multiprocessing import Pool
 import os
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
 
 
-def run(file):
-    basename = os.path.basename(file)
-    print(f"Handling {basename}")
+def progress_bar(files):
+    return tqdm(files, desc="Processing", total=len(files), dynamic_ncols=True)
 
-    src = os.path.join("./data/prod/", basename)
-    dest = os.path.join("./data/prod_backup", basename)
+
+def run(root, basename):
+    filename = f"{root}/{basename}"
+    print(f"Handling {filename}")
+
+    src = filename
+    dest = os.path.join(os.getcwd() + "/data/prod_backup/", basename)
+
+    print(f"Source: {src}")
+    print(f"Destination: {dest}")
+
     subprocess.call(["rsync", "-arq", src, dest])
 
 
-src = "./data/prod/"
-dest = "./data/prod_backup/"
+src = os.getcwd() + "/data/prod/"
+dest = os.getcwd() + "/data/prod_backup/"
 
-for root, _, files in os.walk(src):
-    p = Pool(len(files))
-    p.map(run, files)
+executor = ProcessPoolExecutor()
+
+for root, dirnames, filenames in os.walk(src):
+    for basename in progress_bar(filenames):
+
+        if len(filenames) == 0:
+            continue
+
+        print(root)
+        # print(dirnames)
+        print(filenames)
+
+        # p = Pool(len(filenames))
+        # p.map(run, root, filenames)
+
+        executor.submit(run, root, basename)
