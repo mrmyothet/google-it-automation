c4747dfff44d8f6e66eec63ee3527d78e1e248d3
Myo Thet <mr.myothet@gmail.com>
2024-12-22 02:32:15 +0700

    resource relationship

--- config_mgmt/puppet/ntp.pp
+++ config_mgmt/puppet/ntp.pp
@@ -2,14 +2,16 @@ class ntp {
     package{ 'ntp':
         ensure => latest,
     }
-
     file {'/etc/ntp.conf':
         source => 'puppet:///modules/ntp/ntp.conf'
         replace => true,
+        require => Package['ntp'],
+        notify => Service['ntp'],
     }
-
     service { 'ntp':
         enable => true, 
         ensure => running,
+        require => File['/etc/ntp.conf'],
     }
 }
+include ntp 
