193a520f67a3aa4e07ad8adde8ad3d0371b53299
Myo Thet <mr.myothet@gmail.com>
2024-12-31 23:56:10 +0700

    data serialization formats - json, yaml

---
+++ automating_with_python/web_services/data_serialization.py
@@ -0,0 +1,28 @@
+import json
+
+# pip install pyyaml
+import yaml
+
+
+people = [
+    {
+        "name": "Sabrina Green",
+        "username": "sgreen",
+        "phone": {"office": "802-867-5309", "cell": "802-867-5310"},
+        "department": "IT Infrastructure",
+        "role": "Systems Administrator",
+    },
+    {
+        "name": "Eli Jones",
+        "username": "ejones",
+        "phone": {"office": "684-348-1127"},
+        "department": "IT Infrastructure",
+        "role": "IT Specialist",
+    },
+]
+
+with open("people.json", "w") as people_json:
+    json.dump(people, people_json, indent=2)
+
+with open("people.yaml", "w") as people_yaml:
+    yaml.safe_dump(people, people_yaml)
