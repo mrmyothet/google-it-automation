9cc524dcdfd27990afd9697e3a09aa1cc7de7c06
MyoThet <mr.myothet@gmail.com>
2025-01-02 18:13:48 +0700

    add graphics to pdf using reportlab module

--- automating_with_python/output_generation/generate_pdfs.py
+++ automating_with_python/output_generation/generate_pdfs.py
@@ -4,15 +4,18 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.lib.units import inch
 
 fruit = {
     "elderberry": 1,
-    "figs": 2,
-    "apples": 3,
-    "durian": 4,
+    "figs": 1,
+    "apples": 2,
+    "durians": 3,
     "bananas": 5,
-    "cherries": 6,
-    "grapes": 7,
+    "cherries": 8,
+    "grapes": 13,
 }
 
 report = SimpleDocTemplate("./temp/fruit_report.pdf")
@@ -25,4 +28,16 @@ for k, v in fruit.items():
 table_style = [("GRID", (0, 0), (-1, -1), 1, colors.black)]
 report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
 
-report.build([report_title, report_table])
+report_pie = Pie(width=3 * inch, height=3 * inch)
+report_pie.data = []
+report_pie.labels = []
+for fruit_name in sorted(fruit):
+    report_pie.data.append(fruit[fruit_name])
+    report_pie.labels.append(fruit_name)
+
+# print(report_pie.data)
+# print(report_pie.labels)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+report.build([report_title, report_table, report_chart])
