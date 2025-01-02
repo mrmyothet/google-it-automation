71c454481a3dd96d820b3812bb740ab8dccc47e4
MyoThet <mr.myothet@gmail.com>
2025-01-02 17:32:21 +0700

    adding attachments into a mail message

--- automating_with_python/output_generation/email_Message.py
+++ automating_with_python/output_generation/email_Message.py
@@ -1,4 +1,6 @@
 from email.message import EmailMessage
+import os.path
+import mimetypes
 
 message = EmailMessage()
 
@@ -15,5 +17,20 @@ body = """Hey there!
 I'm learning to send emails using Python!"""
 
 message.set_content(body)
+
+attachment_path = "./temp/education.png"
+attachment_filename = os.path.basename(attachment_path)
+
+mime_type, _ = mimetypes.guess_type(attachment_path)
+mime_type, mime_subtype = mime_type.split("/", 1)
+# print(mime_type, mime_subtype)
+
+with open(attachment_path, "rb") as ap:
+    message.add_attachment(
+        ap.read(),
+        maintype=mime_type,
+        subtype=mime_subtype,
+        filename=os.path.basename(attachment_path),
+    )
 
 print(message)
