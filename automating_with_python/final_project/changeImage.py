6c71979950473e53e503e5a805056a2abc57ba8a
Myo Thet <mr.myothet@gmail.com>
2025-01-04 19:39:56 +0700

    add changeImage.py

--- automating_with_python/final_project/changeImage.py
+++ automating_with_python/final_project/changeImage.py
@@ -7,4 +7,14 @@ root_path = os.getcwd()
 folder_path = os.path.join(root_path, "supplier-data/images")
 
 for filename in os.listdir(folder_path):
-    print(filename)
+    # print(filename)
+    if filename == "LICENSE" or filename == "README":
+        continue
+    img = Image.open(os.path.join(folder_path, filename))
+    # print(img.filename, img.format, img.size)
+    new_img = img.resize((600, 400)).convert("RGB")
+
+    new_filename = os.path.join(folder_path, filename.split(".")[0] + ".jpeg")
+    print(new_filename)
+    new_img.save(new_filename)
+    img.close()
