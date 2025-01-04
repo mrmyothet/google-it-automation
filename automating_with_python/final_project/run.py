1cab70e18837afc2bb9e9b5e3cea42406fede671
Myo Thet <mr.myothet@gmail.com>
2025-01-04 19:48:11 +0700

    final project - generate pdf, send email and health check

--- automating_with_python/final_project/run.py
+++ automating_with_python/final_project/run.py
@@ -1,17 +1,32 @@
 #! /usr/bin/env python3
-
 import os
 import requests
 
-root_path = os.getcwd()
-folder_path = os.path.join(root_path, "supplier-data/descriptions")
-
-for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
-    with open(file_path, "r") as file:
-        lines = file.readlines()
-        # print(lines)
-        name = lines[0]
-        weight = lines[1]
-        description = lines[2]
-        print(name, weight, description)
+fruits = {}
+keys = ["name", "weight", "description", "image_name"]
+index = 0
+path = "./supplier-data/descriptions/"
+img_path = "./supplier-data/images/"
+for file in os.listdir("./supplier-data/descriptions"):
+    with open(path + file) as f:
+        for ln in f:
+            line = ln.strip()
+            if "lbs" in line:
+                nline = line.split()
+                wght = int(nline[0])
+                fruits["weight"] = wght
+                index += 1
+            else:
+                try:
+                    fruits[keys[index]] = line
+                    index += 1
+                except:
+                    fruits[keys[2]] = line
+        index = 0
+        split_f = file.split(".")
+        name = split_f[0] + ".jpeg"
+        for fle in os.listdir("./supplier-data/images"):
+            if fle == name:
+                fruits["image_name"] = name
+        response = requests.post("http://<External_IP>/fruits/", json=fruits)
+        fruits.clear()
